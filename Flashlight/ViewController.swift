c862b4b185d57d9026bb44add5e3c73c29562621
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-07-08 11:11:43 -0500

    added button for light

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Global var and let
+    var lightOn: Bool = true
+    
+    //Outlets
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    //Actions
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        lightOn.toggle()
+        if lightOn {
+            view.backgroundColor = .white
+            sender.setTitle("TURN OFF", for: .normal)
+        } else {
+            view.backgroundColor = .black
+            sender.setTitle("TURN ON", for: .normal)
+        }
+        
+    }
+    
 
 }
 
